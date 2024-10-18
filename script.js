addbf73993859f5b6aaffb5ff616ea08e79a45b6
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-10-18 18:43:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,50 +1,31 @@
-// class Person {
-//     constructor(name, age) {
-//         this._name = name;
-//         this._age = age;
-//     }
+class Person {
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
 
-//     greet() {
-//         console.log(`Hello, my name is ${this._name} and I am ${this._age} years old`);
-//     }
-// }
-
-// class Employee extends Person {
-//     constructor(name, age, jobTitle) {
-//         // Call the parent constructor
-//         super(name, age);
-//         this._jobTitle = jobTitle;
-//     }
-
-//     jobGreet() {
-//         console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}`);
-//     }
-// }
-
-
-// const p=new Person("Alice",25);
-// p.greet();
-// const e=new Employee("Bob",30,"Manager");
-// e.jobGreet();
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
+    greet() {
+        console.log(`Hello, my name is ${this._name} and I am ${this._age} years old.`);
+    }
 }
-Person.prototype.greet = function() {
-  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
-};
- 
- 
-function Employee(name, age, jobTitle) {
-  Person.call(this, name, age);
-  this.jobTitle = jobTitle;
+
+class Employee extends Person {
+    constructor(name, age, jobTitle) {
+        // Call the parent constructor
+        super(name, age);
+        this._jobTitle = jobTitle;
+    }
+
+    jobGreet() {
+        console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`);
+    }
 }
- 
-Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.constructor = Employee;
- 
-Employee.prototype.jobGreet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-};
+
+
+const p=new Person("Alice",25);
+p.greet();
+const e=new Employee("Bob",30,"Manager");
+e.jobGreet();
+
 window.Person = Person;
 window.Employee = Employee;
