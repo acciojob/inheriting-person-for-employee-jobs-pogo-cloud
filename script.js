c4e381791b93e5da50f9ebdc7bf1ba1ded14ec27
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-10-18 16:19:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-// complete this js code
-function Person(name, age) {}
+class Person {
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
 
-function Employee(name, age, jobTitle) {}
+    greet() {
+        return `Hello, my name is ${this._name}, I am ${this._age} years old`;
+    }
+}
+
+class Employee extends Person {
+    constructor(name, age, jobTitle) {
+        // Call the parent constructor
+        super(name, age);
+        this._jobTitle = jobTitle;
+    }
+
+    jobGreet() {
+        return `Hello, my name is ${this._name}, I am ${this._age} years old and I am a ${this._jobTitle}`;
+    }
+}
+
 
 // Do not change code below this line
 window.Person = Person;
